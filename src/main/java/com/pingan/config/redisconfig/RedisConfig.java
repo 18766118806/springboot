d97fe3a4fd4b19430a5b4a472cef86994645744b
Jun <1025457001@qq.com>
2019-05-30 19:15:08 +0800

    测试

---
+++ src/main/java/com/pingan/config/redisconfig/RedisConfig.java
@@ -0,0 +1,11 @@
+package com.pingan.config.redisconfig;
+
+/*
+ * @Author:  Yajun_Xu
+ * @Create: 2019-05-30 19:14
+ **/
+public class RedisConfig {
+    public static void main(String[] args) {
+        System.out.println ("test");
+    }
+}
